f998b203bc1384e2162c0eb4d98d2dba6579a8bc
Arpit yadav <yadavarpit1819@gmail.com>
2024-04-21 15:08:24 +0530

    And also add color and seterror function in javascript

--- script.js
+++ script.js
@@ -6,3 +6,77 @@ function normalImg(x){
     x.style.height = "7.5rem";
     x.style.width = "12rem";
 }
+function validationForm(){  
+    var returnval=true;
+
+
+        const userName=document.myform.Name.value;
+        // console.log(userName);
+        // console.log(userName.length);
+        if(userName.length<5){
+            returnval = false;
+         seterror('username',"Name cannot be empty",returnval,"unerror");
+         
+        }
+
+        const userContact=document.myform.contact.value;
+        if(isNaN(userContact)){
+            returnval=false;
+            seterror('contact',"Enter numeric value only",returnval);
+            } 
+        if(userContact.length<10 || userContact.length>11){
+            returnval = false;
+            seterror('contact',"Enter a valid mobile number",returnval,'ucerror');
+            
+        }
+        
+        const userEmail=document.myform.email.value;
+        var atposition=userEmail.indexOf("@");
+        var dotposition=userEmail.lastIndexOf(".");
+        if(atposition<1 || dotposition<atposition+2 || dotposition+2>=userEmail.length){
+            returnval=false;
+            seterror('mail',"Please Enter a valid Email address",returnval,"ueerror");
+            
+        }
+
+        const userpasswd=document.myform.Password.value;
+        if(userpasswd.length<=1 || userpasswd.length<5 || userpasswd.length>12){
+            returnval=false;
+           seterror('passwd',"Please Enter atleast 6 character Password",returnval,"uperror");
+        }
+   
+    // return returnval; 
+    
+    // document.getElementById('regform').addEventListener('submit', function(e) {
+    //         e.preventDefault();
+    
+        // validate fields
+        // checkUsername();
+        // checkContact();
+        // checkEmail();
+        // checkPassword();
+    
+        // let isvalidForm = isUsernameValid &&
+        //     isContactvalid &&
+        //     isEmailValid &&
+        //     isPasswordValid;
+    // });
+    return returnval;
+    
+}
+//seterror function call here
+var seterror=function seterror(id,error,returnval,id1){
+    
+    // console.log(id);
+    // console.log(error);
+    console.log(returnval);
+    
+    if (returnval===false){
+        console.log(document.getElementById(id));
+        console.log(document.getElementById(id1).innerHTML=error);
+    }else{
+        returnval=false;
+    }
+
+
+}
